4d009adc16ba8d844554abdfd468c5914a388520
Ellpeck <megamaximal@gmail.com>
2019-05-19 12:32:38 +0200

    added fake walls

---
+++ Assets/Scripts/FakeWall.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UnityEngine;
+
+public class FakeWall : MonoBehaviour {
+
+    private static readonly int Disappear = Animator.StringToHash("Disappear");
+    private Animator animator;
+
+    private void Start() {
+        this.animator = this.GetComponent<Animator>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        if (other.CompareTag("Player"))
+            this.animator.SetTrigger(Disappear);
+    }
+
+}
